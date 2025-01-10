ee8d332cfd67c716e7a670c7c665a24935ee6976
HarshForreal <hp673315@gmail.com>
2025-01-10 17:39:43 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    orgin: "*",
+    orgin: "https://expense-tracker-qn11.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
